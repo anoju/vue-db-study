2e885026f9140991e62e05ede12594cde880907c
zun5761 <zun5761@gmail.com>
2021-09-09 21:46:45 +0900

    ahj 재업로드

--- ahj/src/main.js
+++ ahj/src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import './assets/scss/front.scss'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>',
-})
+  store,
+  render: (h) => h(App)
+}).$mount('#app')
