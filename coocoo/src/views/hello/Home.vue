4ae836894e0c285d5c626c2df66d2da7cf4c7480
coocoo <lry8471@gmail.com>
2021-09-25 17:28:49 +0900

    20210925

---
+++ coocoo/src/views/hello/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div
+      class="logo_wrap"
+      @click="isBg = !isBg"
+    >
+      <div
+        v-if="isBg"
+        class="logo"
+      />
+      <img
+        v-else
+        alt="Vue logo"
+        src="@/assets/images/logo.png"
+      >
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      isBg: false,
+    }
+  },
+}
+</script>
